e812193d0d74d4214826a65244d576a21c519dc8
Batiatari Kaze <batiatari.dono@gmail.com>
2015-11-30 18:33:56 +0200

    Two fragments

---
+++ app/src/main/java/com/binarnahata/rainbowkingdom/Fragments/GameFragment.java
@@ -0,0 +1,35 @@
+package com.binarnahata.rainbowkingdom.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.binarnahata.rainbowkingdom.R;
+
+/**
+ * RainbowKingdom
+ * Created on 30.11.15, 18:07
+ *
+ * @author bat
+ * @version 0.1
+ */
+public class GameFragment extends Fragment {
+	/* КОНСТАНТЫ И ПЕРЕМЕННЫЕ */
+	/* КОНСТАНТЫ И ПЕРЕМЕННЫЕ */
+	/* ГЕТТЕРЫ И СЕТТЕРЫ */
+	/* ГЕТТЕРЫ И СЕТТЕРЫ */
+	/* КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ */
+	public GameFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_main, container, false);
+	}
+	/* КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ */
+	/* МЕТОДЫ */
+	/* МЕТОДЫ */
+}
