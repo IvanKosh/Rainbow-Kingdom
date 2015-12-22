bc93a35b10b7bf8963ff26a65c4341e4489aec74
Batiatari Kaze <batiatari.dono@gmail.com>
2015-12-22 18:41:05 +0200

    База для ачивок

---
+++ app/src/main/java/com/binarnahata/rainbowkingdom/Models/Achievement/Achievement.java
@@ -0,0 +1,20 @@
+package com.binarnahata.rainbowkingdom.Models.Achievement;
+
+/**
+ * RainbowKingdom
+ * Created on 22.12.15, 18:33
+ *
+ * @author bat
+ * @version 0.1
+ */
+public class Achievement {
+	/* КОНСТАНТЫ И ПЕРЕМЕННЫЕ */
+	private static final String TAG = Achievement.class.getSimpleName();
+	/* КОНСТАНТЫ И ПЕРЕМЕННЫЕ */
+	/* ГЕТТЕРЫ И СЕТТЕРЫ */
+	/* ГЕТТЕРЫ И СЕТТЕРЫ */
+	/* КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ */
+	/* КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ */
+	/* МЕТОДЫ */
+	/* МЕТОДЫ */
+}
